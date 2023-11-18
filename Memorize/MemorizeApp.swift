1b0f394fbaf9a8d83ea174898fcd306596aa2525
dk150 <katarina.dokic@gmail.com>
2023-11-18 19:46:16 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Katarina on 18.11.23..
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
